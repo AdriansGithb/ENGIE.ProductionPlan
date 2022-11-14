69a554c48caae5c20d26191c4d3f583fa15df1df
Adrian <Adrian.vanhoeke@satellit.be>
2022-11-14 13:42:40 +0100

    Add models

--- ProductionPlan.Core/Models/PowerGenerationUnit.cs
+++ ProductionPlan.Core/Models/PowerGenerationUnit.cs
@@ -7,12 +7,15 @@ using System.Threading.Tasks;
 
 namespace ProductionPlan.Core.Models
 {
-    public class Powerplant
+    public class PowerGenerationUnit
     {
         public string Name { get; set; }
         public PowerplantTypeEnum Type { get; set; }
         public decimal Efficiency { get; set; }
         public decimal PMin { get; set; }
         public decimal PMax { get; set; }
+        public decimal AdvisedProduction { get; set; } = 0;
+        public decimal ProductionCostPerUnit { get; set; }
+
     }
 }
--- ProductionPlan.Core/Services/ProductionService.cs
+++ ProductionPlan.Core/Services/ProductionService.cs
@@ -1,4 +1,6 @@
-﻿using ProductionPlan.Core.Abstract;
+﻿using Microsoft.Extensions.Logging;
+using ProductionPlan.Core.Abstract;
+using ProductionPlan.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,27 @@ namespace ProductionPlan.Core.Services
 {
     public class ProductionService : IProductionService
     {
+        private readonly ILogger<ProductionService> _logger;
 
+        public ProductionService(ILogger<ProductionService> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog injected into ProductionService");
+        }
+
+        public void PlanProduction(Payload payload)
+        {
+            try
+            {
+                // créer un response powerplant
+                // créer un powerplant pour le calcul de prod
+                // si le load est supérieur au max : renvoyer tout au max
+                // sinon calculer le load
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }

703ade6a609ea59e6fed69736dcf5ced567adfeb
Adrian <Adrian.vanhoeke@satellit.be>
2022-11-17 10:07:38 +0100

    Add more scenarii unit tests

--- ProductionPlan.Core.Test/ProductionPlanScenariiTest.cs
+++ ProductionPlan.Core.Test/ProductionPlanScenariiTest.cs
@@ -58,5 +58,114 @@ namespace ProductionPlan.Core.Test
             Assert.Throws<ArgumentException>(() => _service.PlanProduction(productionPlan));
         }
 
+        [Fact]
+        public void ProductionPlan_Wind_Enough()
+        {
+            Payload productionPlan = new Payload
+            {
+                Load = 25,
+                Fuels = _baseEnergyMetrics,
+                Powerplants = new List<Powerplant>
+                {
+                new Powerplant{ Name = "Gas1", Type = PowerplantTypeEnum.gasfired , Efficiency = 0.5M, PMin = 10, PMax = 100 },
+                new Powerplant{ Name = "Wind1", Type = PowerplantTypeEnum.windturbine , Efficiency = 1, PMin = 0, PMax = 50 }
+                }
+            };
+
+
+            var result = _service.PlanProduction(productionPlan).ToList();
+
+            Assert.Equal(25, result.First(x => x.Name == "Wind1").P);
+            Assert.Equal(0, result.First(x => x.Name == "Gas1").P);
+        }
+
+        [Fact]
+        public void PlanProduction_Wind_NotEnough()
+        {
+            Payload productionPlan = new Payload
+            {
+                Load = 50,
+                Fuels = _baseEnergyMetrics,
+                Powerplants = new List<Powerplant>
+                {
+                new Powerplant{ Name = "Gas1", Type = PowerplantTypeEnum.gasfired , Efficiency = 0.5M, PMin = 10, PMax = 100 },
+                new Powerplant{ Name = "Wind1", Type = PowerplantTypeEnum.windturbine , Efficiency = 1, PMin = 0, PMax = 50 }
+                }
+            };
+
+            var result = _service.PlanProduction(productionPlan).ToList();
+
+            Assert.Equal(25, result.First(x => x.Name == "Wind1").P);
+            Assert.Equal(25, result.First(x => x.Name == "Gas1").P);
+        }
+
+        [Fact]
+        public void PlanProduction_Wind_TooMuch()
+        {
+            Payload productionPlan = new Payload
+            {
+                Load = 20,
+                Fuels = _baseEnergyMetrics,
+                Powerplants = new List<Powerplant>
+                {
+                new Powerplant{ Name = "Gas1", Type = PowerplantTypeEnum.gasfired , Efficiency = 0.5M, PMin = 10, PMax = 100 },
+                new Powerplant{ Name = "Wind1", Type = PowerplantTypeEnum.windturbine , Efficiency = 1, PMin = 0, PMax = 50 }
+                }
+            };
+
+            var result = _service.PlanProduction(productionPlan).ToList();
+
+            Assert.Equal(0, result.First(x => x.Name == "Wind1").P);
+            Assert.Equal(20, result.First(x => x.Name == "Gas1").P);
+        }
+
+        [Fact]
+        public void PlanProduction_Gas_Efficiency()
+        {
+            Payload productionPlan = new Payload
+            {
+                Load = 20,
+                Fuels = _baseEnergyMetrics,
+                Powerplants = new List<Powerplant>
+                {
+                new Powerplant{ Name = "Gas1", Type = PowerplantTypeEnum.gasfired , Efficiency = 0.5M, PMin = 10, PMax = 100 },
+                new Powerplant{ Name = "Gas2", Type = PowerplantTypeEnum.gasfired , Efficiency = 0.6M, PMin = 10, PMax = 100 },
+                new Powerplant{ Name = "Gas3", Type = PowerplantTypeEnum.gasfired , Efficiency = 0.8M, PMin = 10, PMax = 100 },
+                new Powerplant{ Name = "Gas4", Type = PowerplantTypeEnum.gasfired , Efficiency = 0.3M, PMin = 10, PMax = 100 },
+                new Powerplant{ Name = "Gas5", Type = PowerplantTypeEnum.gasfired , Efficiency = 0.45M, PMin = 10, PMax = 100 },
+                }
+            };
+
+            var result = _service.PlanProduction(productionPlan).ToList();
+
+            Assert.Equal(20, result.First(x => x.Name == "Gas3").P);
+            Assert.Equal(0, result.Where(x => x.Name != "Gas3").Select(x => x.P).Sum());
+        }
+
+        [Fact]
+        public void PlanProduction_Gas_AllNeeded()
+        {
+            Payload productionPlan = new Payload
+            {
+                Load = 490,
+                Fuels = _baseEnergyMetrics,
+                Powerplants = new List<Powerplant>
+                {
+                new Powerplant{ Name = "Gas1", Type = PowerplantTypeEnum.gasfired , Efficiency = 0.5M, PMin = 10, PMax = 100 },
+                new Powerplant{ Name = "Gas2", Type = PowerplantTypeEnum.gasfired , Efficiency = 0.6M, PMin = 10, PMax = 100 },
+                new Powerplant{ Name = "Gas3", Type = PowerplantTypeEnum.gasfired , Efficiency = 0.8M, PMin = 10, PMax = 100 },
+                new Powerplant{ Name = "Gas4", Type = PowerplantTypeEnum.gasfired , Efficiency = 0.3M, PMin = 10, PMax = 100 },
+                new Powerplant{ Name = "Gas5", Type = PowerplantTypeEnum.gasfired , Efficiency = 0.45M, PMin = 10, PMax = 100 },
+                }
+            };
+
+            var result = _service.PlanProduction(productionPlan).ToList();
+
+            Assert.Equal(100, result.First(x => x.Name == "Gas1").P);
+            Assert.Equal(100, result.First(x => x.Name == "Gas2").P);
+            Assert.Equal(100, result.First(x => x.Name == "Gas3").P);
+            Assert.Equal(90, result.First(x => x.Name == "Gas4").P);
+            Assert.Equal(100, result.First(x => x.Name == "Gas5").P);
+        }
     }
 }
